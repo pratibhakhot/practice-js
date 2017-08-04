c6e70da1201aa643200fb4276813d9dde1832c4b
unknown <you@example.com>
2017-08-04 22:46:05 +0530

    push

--- calculator-app/app.js
+++ calculator-app/app.js
@@ -1,11 +1,11 @@
 
 var  app_root = __dirname,
-
+console.log(app_root);
 express=require('express');
 var app=express();
 
 
-
+app.use(express.static(app_root+'/site/public'));
 app.get('/',function(req,res){
 	/*res.send('hello world');*/
 	   res.sendFile('./site/index.html',{root:app_root});
